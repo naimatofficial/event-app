e2b08cfa7d73f2fe489efdbe5db15ba6d13dbfe6
Naimat Ullah <naimat.official.10@gmail.com>
2024-01-31 17:17:59 +0500

    add types and utils functions

---
+++ lib/database/index.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+// Cached database connection
+const cached = (global as any).mongoose || { conn: null, promise: null };
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+export const connectToDatabase = async () => {
+	// If a connection already exists, return it
+	if (cached.conn) return cached.conn;
+
+	if (!MONGODB_URI) throw new Error("MONGODB_URI is missing!");
+
+	// If a promise to connect to the database already exists, reuse it;
+	// otherwise, create a new promise and connect to the database
+	cached.promise =
+		cached.promise ||
+		mongoose.connect(MONGODB_URI, {
+			dbName: "evently",
+			bufferCommands: false,
+		});
+
+	// Wait for the database connection to be established and assign it to the cached connection
+	cached.conn = await cached.promise;
+
+	// Return the established database connection
+	return cached.conn;
+};
